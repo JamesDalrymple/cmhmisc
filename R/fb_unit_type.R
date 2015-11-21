5985a0fa2cd5a459d32d8f9e1f3efd101f970a89
James Dalrymple <jamesdalrymple@hotmail.com>
2015-11-21 01:50:51 -0500

    add dataset fb_unit_type

---
+++ R/fb_unit_type.R
@@ -0,0 +1,23 @@
+#' Funding Bucket Unit Type Data
+#'
+#' Encompass database is lacking unit type data for SALs, this fixes that
+#' issue. Subject to update when necessary. Last updated 11/3/2015.
+#'
+#' @docType data
+#'
+#' @usage data(fb_unit_type)
+#'
+#' @format A data.table object.
+#'
+#' @keywords datasets
+#'
+#' @source Funding Bucket Report from PCE.
+#'
+#' @examples
+#' \dontrun{
+#' data(fb_unit_type)
+#' rm("fb_unit_type")
+#' fb_unit_type
+#' wccmh::fb_unit_type
+#' }
+"fb_unit_type"
